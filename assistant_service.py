260d438ba7b4938d77c2751594e756f1ce6f29e2
Trần Tín <ttins2004@gmail.com>
2024-04-04 14:54:39 +0700

    last push

--- assistant_service.py
+++ assistant_service.py
@@ -1,13 +1,29 @@
 from openai import OpenAI
 import datetime
+import requests
 
-client = OpenAI(api_key="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")
+api_link = "https://84f66vbk-3000.asse.devtunnels.ms"  # Replace with your actual API URL
+client = OpenAI(api_key="") #API openAI
+
+def control_API(api_link, led_topic, action):
+  url = f"{api_link}/api/control/{action}"  # Construct the full URL
+  headers = {'Content-Type': 'application/json'}
+  data = {'topic': led_topic}  # Payload data for the PUT request
+
+  try:
+    response = requests.put(url, headers=headers, json=data)
+    response.raise_for_status()  # Raise an exception for non-200 status codes
+    return response
+  except requests.exceptions.RequestException as error:
+    print(f"Error controlling LED: {error}")
+    return None
 
 def gptService(userContent):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a useful virtual assistant in the smart home."},
+            {"role": "system", "content": "Our house is at Vietnam Aviation Academy campus 2."},
             {"role": "system", "content": "You answer briefly in one sentence."},
             {"role": "user", "content": userContent}
         ],
@@ -25,59 +41,153 @@ def time():
 
 statusLightAndFan = [0, 0] #đèn, quạt
 
-def assistantModel(userContent):
+def assistantModel_vi(userContent):
     if userContent == "":
         answer = "Xin lỗi, tôi không nghe rõ"
-    elif (("nhóm 4" in userContent) or ("nhóm bốn" in userContent)) :
-        answer = "Tôi vẫn đang hoạt động."
-    elif "xin chào" in userContent:   
-        answer = "Xin chào, tôi là trợ lý ảo nhà thông minh."
-    elif ((("mấy" in userContent) or ("bao nhiêu" in userContent)) and ("giờ" in userContent)     
-        or (("thời gian" in userContent) and ("hiện tại" in userContent))):
-        answer = "Bây giờ là " + str(time()[0]) + " giờ " + str(time()[1]) + " phút."
-    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
-          and (("ngày" in userContent) or ("buổi" in userContent) or ("tháng" in userContent) or ("năm" in userContent))
-          and (("hiện" in userContent) or ("nay" in userContent))
-          and (("âm" in userContent) or ("ta" in userContent))):
-        answer = "Xin lỗi tôi không có kiến thức sử dụng lịch âm, xin lỗi vì sự bất tiện."
-    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
-          and (("ngày" in userContent) or ("buổi" in userContent) or ("tháng" in userContent) or ("năm" in userContent))
-          and (("hiện" in userContent) or ("nay" in userContent))):
-        answer = "Hôm nay là ngày " + str(time()[2]) + " tháng " + str(time()[3]) + " năm " + str(time()[4]) + "."
-    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
-          and (("thứ" in userContent) or (("ngày" in userContent) and ("tuần" in userContent)))
-          and (("hiện" in userContent) or ("nay" in userContent))):
-        answer = gptService("ngày " + str(time()[2]) + " tháng " + str(time()[3]) + " năm " + str(time()[4]) + "là thứ mấy")
-    elif ("bạn" in userContent) and (("tên" in userContent) or ("là" in userContent) or ("gì" in userContent)):
-        answer = "Tôi là trợ lý ảo do nhóm 4 tạo ra tên là BÁO."
-    elif ("quạt" in userContent) or ("đèn" in userContent):
-        if ("bật" in userContent) and ("quạt" in userContent) and ("đèn" in userContent):
+    elif (("quạt" in userContent) or ("đèn" in userContent)) or (("quat" in userContent) or ("den" in userContent)):
+        if (("bật" in userContent) and ("quạt" in userContent) and ("đèn" in userContent)) or (("bat" in userContent) and ("quat" in userContent) and ("den" in userContent)):
             answer = "Tôi đã bật quạt và đèn."
-            statusLightAndFan[0] = 1
-            statusLightAndFan[1] = 1
-        elif ("bật" in userContent) and ("quạt" in userContent) and ("đèn" in userContent):
+
+            response = control_API(api_link, "led01", "led-on")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "led02", "led-on")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "fan01", "fan-on")
+            if response:
+                print("FAN turned on successfully!")
+            response = control_API(api_link, "fan02", "fan-on")
+            if response:
+                print("FAN turned on successfully!")
+        elif (("tắt" in userContent) and ("quạt" in userContent) and ("đèn" in userContent)) or (("tat" in userContent) and ("quat" in userContent) and ("den" in userContent)):
             answer = "Tôi đã tắt quạt và đèn."
-            statusLightAndFan[0] = 0
-            statusLightAndFan[1] = 0
-        elif ("bật" in userContent) and ("đèn" in userContent):
+
+            response = control_API(api_link, "led01", "led-off")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "fan01", "fan-off")
+            if response:
+                print("FAN turned on successfully!")
+            response = control_API(api_link, "led02", "led-off")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "fan02", "fan-off")
+            if response:
+                print("FAN turned on successfully!")
+        elif (("bật" in userContent) and ("đèn" in userContent)) or (("bat" in userContent) and ("den" in userContent)):
             answer = "Tôi đã bật đèn."
-            statusLightAndFan[0] = 1
-        elif ("tắt" in userContent) and ("đèn" in userContent):
+
+            response = control_API(api_link, "led01", "led-on")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "led02", "led-on")
+            if response:
+                print("LED turned on successfully!")
+        elif (("tắt" in userContent) and ("đèn" in userContent)) or (("tat" in userContent) and ("den" in userContent)):
             answer = "Tôi đã tắt đèn."
-            statusLightAndFan[0] = 0
-        elif ("bật" in userContent) and ("quạt" in userContent):
+
+            response = control_API(api_link, "led01", "led-off")
+            if response:
+                print("LED turned on successfully!")
+            response = control_API(api_link, "led02", "led-off")
+            if response:
+                print("LED turned on successfully!")
+        elif (("bật" in userContent) and ("quạt" in userContent)) or (("bat" in userContent) and ("quat" in userContent)):
             answer = "Tôi đã bật quạt."
-            statusLightAndFan[1] = 1
-        elif ("tắt" in userContent) and ("quạt" in userContent):
+
+            response = control_API(api_link, "fan01", "fan-on")
+            if response:
+                print("FAN turned on successfully!")
+            response = control_API(api_link, "fan02", "fan-on")
+            if response:
+                print("FAN turned on successfully!")
+        elif (("tắt" in userContent) and ("quạt" in userContent)) or (("tat" in userContent) and ("quat" in userContent)):
             answer = "Tôi đã tắt quạt."
-            statusLightAndFan[1] = 0
+
+            response = control_API(api_link, "fan01", "fan-off")
+            if response:
+                print("FAN turned on successfully!")
+            response = control_API(api_link, "fan02", "fan-off")
+            if response:
+                print("FAN turned on successfully!")
         else:
             answer = "Tôi có thể điều chỉnh trạng thái hai thiết bị này, bạn có thể nói rõ ràng hơn."
         
-        print(statusLightAndFan)
-    elif ("tạm biệt" in userContent):
-        answer = "Tạm biệt và hẹn gặp lại."
+    elif ("chào" in userContent) or ("chao" in userContent):   
+        answer = "Xin chào, tôi là trợ lý ảo nhà thông minh."
+    elif ((("mấy" in userContent) or ("nhiêu" in userContent) or ("khoảng" in userContent) or ("hiện" in userContent)) 
+          and (("giờ" in userContent) or ("thời gian" in userContent) or ("phút" in userContent) or ("giây" in userContent))) or ((("may" in userContent) or ("nhieu" in userContent) or ("khoang" in userContent) or ("hien" in userContent)) 
+          and (("gio" in userContent) or ("thoi gian" in userContent) or ("phut" in userContent) or ("giay" in userContent))):
+        answer = "Hiện tại là " + str(time()[0]) + " giờ " + str(time()[1]) + " phút theo giờ Việt Nam."
+    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
+          and (("ngày" in userContent) or ("buổi" in userContent) or ("tháng" in userContent) or ("năm" in userContent))
+          and (("hiện" in userContent) or ("nay" in userContent))
+          and (("âm" in userContent) or ("ta" in userContent))) or ((("may" in userContent) or ("nao" in userContent) or ("la" in userContent)) 
+          and (("ngay" in userContent) or ("buoi" in userContent) or ("thang" in userContent) or ("nam" in userContent))
+          and (("hien" in userContent) or ("nay" in userContent))
+          and (("am" in userContent) or ("ta" in userContent))):
+        answer = "Xin lỗi tôi không có kiến thức sử dụng lịch âm, xin lỗi vì sự bất tiện."
+    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
+          and (("ngày" in userContent) or ("buổi" in userContent) or ("tháng" in userContent) or ("năm" in userContent))
+          and (("hiện" in userContent) or ("nay" in userContent))) or ((("may" in userContent) or ("nao" in userContent) or ("la" in userContent)) 
+          and (("ngay" in userContent) or ("buoi" in userContent) or ("thang" in userContent) or ("nam" in userContent))
+          and (("hien" in userContent) or ("nay" in userContent))):
+        answer = "Hôm nay là ngày " + str(time()[2]) + " tháng " + str(time()[3]) + " năm " + str(time()[4]) + "."
+    elif ((("mấy" in userContent) or ("nào" in userContent) or ("là" in userContent)) 
+          and (("thứ" in userContent) or (("ngày" in userContent) and ("tuần" in userContent)))
+          and (("hiện" in userContent) or ("nay" in userContent))) or ((("may" in userContent) or ("bao" in userContent) or ("la" in userContent)) 
+          and (("thu" in userContent) or (("ngay" in userContent) and ("tuan" in userContent)))
+          and (("hien" in userContent) or ("nay" in userContent))):
+        answer = gptService("ngày " + str(time()[2]) + " tháng " + str(time()[3]) + " năm " + str(time()[4]) + "là thứ mấy")
+    elif (("bạn" in userContent) and (("tên" in userContent) or ("là" in userContent) or ("gì" in userContent))) or (("ban" in userContent) and (("ten" in userContent) or ("la" in userContent) or ("gi" in userContent))):
+        answer = "Tôi là trợ lý ảo do nhóm 4 tạo ra, tôi tên là SmartHome"
+      
+    else:
+        answer = gptService(userContent)
 
+    return answer
+
+def assistantModel_en(userContent):
+    if userContent == "":
+        answer = "Sorry, I didn't hear clearly"
+    elif (("fan" in userContent) or ("light" in userContent)):
+        if (("on" in userContent) and ("fan" in userContent) and ("light" in userContent)):
+            answer = "I turned on the fan and lights."
+        elif (("off" in userContent) and ("fan" in userContent) and ("light" in userContent)):
+            answer = "I turned off the fan and lights."
+        elif (("on" in userContent) and ("light" in userContent)):
+            answer = "I turned on the light."
+        elif (("off" in userContent) and ("light" in userContent)):
+            answer = "I turned off the light."
+        elif (("on" in userContent) and ("fan" in userContent)):
+            answer = "I turned on the fan."
+        elif (("off" in userContent) and ("fan" in userContent)):
+            answer = "I turned off the fan."
+        else:
+            answer = "I can adjust the status of these two devices, can you say it more clearly."
+
+    elif ("hello" in userContent) or ("hi" in userContent):
+        answer = "Hello, I'm a smart home virtual assistant."
+    elif ((("how many" in userContent) or ("how much" in userContent) or ("about" in userContent) or ("show" in userContent) or ("what" in userContent))
+        and (("hours" in userContent) or ("time" in userContent) or ("minutes" in userContent) or ("seconds" in userContent))):
+        answer = "The current time is " + str(time()[0]) + " hours " + str(time()[1]) + " minutes in Vietnam time."
+    elif ((("which" in userContent) or ("which" in userContent) or ("is" in userContent))
+        and (("day" in userContent) or ("session" in userContent) or ("month" in userContent) or ("year" in userContent))
+        and (("now" in userContent) or ("now" in userContent))
+        and (("negative" in userContent))):
+        answer = "Sorry I don't have the knowledge to use the lunar calendar, sorry for the inconvenience."
+    elif ((("which" in userContent) or ("which" in userContent) or ("is" in userContent))
+        and (("day" in userContent) or ("session" in userContent) or ("month" in userContent) or ("year" in userContent))
+        and (("now" in userContent) or ("now" in userContent))):
+        answer = "Today is the day " + str(time()[2]) + " month " + str(time()[3]) + " year " + str(time()[4]) + "."
+    elif ((("which" in userContent) or ("which" in userContent) or ("is" in userContent))
+        and (("day" in userContent) or (("day" in userContent) and ("week" in userContent)))
+        and (("now" in userContent) or ("now" in userContent))): 
+        answer = gptService("day " + str(time()[2]) + "month" + str(time()[3]) + "year" + str(time()[4]) + "what day is it ")
+    elif (("name" in userContent) and (("you" in userContent) or ("is" in userContent) or ("what" in userContent))):
+        answer = "I am a virtual assistant created by group 4, my name is SmartHome."
+    
     else:
         answer = gptService(userContent)
 
--- voice_assistant.py
+++ voice_assistant.py
@@ -1,17 +1,14 @@
-import pyttsx3
-import speech_recognition
+import asyncio
+import json
+import websockets
 import enchant
-import keyboard
 
-from assistant_service import assistantModel
+from assistant_service import assistantModel_vi, assistantModel_en
 
-voiceAssistant = pyttsx3.init()
-voices = voiceAssistant.getProperty("voices")
-voiceAssistant.setProperty("voice", voices[1].id)
 
 checkEn = enchant.Dict("en_US")
 
-def checkEnService(content) -> bool:
+def checkEnService(content) -> str:
     temp = content.split()
     count_vi = 0
     count_en = 0
@@ -22,60 +19,43 @@ def checkEnService(content) -> bool:
             count_vi += 1
     
     if  count_vi < count_en:
-        return True
+        language = "en"
+    else:
+        language = "vi"
     
-    return False
+    return language
 
-def assistantSpeak(audio):
-    voiceAssistant.say(audio)
-    voiceAssistant.runAndWait()
+def assistant(userContent):
+    language = checkEnService(userContent)
 
-def userCommand():
-    assistant_ear = speech_recognition.Recognizer()
+    if language == "vi":
+        userContent = userContent.lower()
+        answer = assistantModel_vi(userContent)
+        return answer
+        
+    elif language == "en":
+        userContent = userContent.lower()
+        answer = assistantModel_en(userContent)
+        return answer
 
-    with speech_recognition.Microphone() as mic:
-        assistant_ear.dynamic_energy_threshold = True
-        assistant_ear.dynamic_energy_adjustment_damping = 0.5
-        assistant_ear.dynamic_energy_adjustment_ratio = 1.5
+async def webSocketService(websocket, path):
+    async for message in websocket:
+        data = json.loads(message)
+        text = data["text"]
+        # print(text)
+        
+        answerMessage = assistant(text)
+        language = checkEnService(answerMessage)
 
-        audio = assistant_ear.listen(mic)
-        assistant_ear.phrase_threshold = 10
+        response_data = {"status": "success", "message": [answerMessage, language]}
 
-    try:
-        query = assistant_ear.recognize_google(audio, language='vi')
-    except:
-        query = ""
-    
-    return query
+        await websocket.send(json.dumps(response_data))
 
-def assistant():
-    print("Assistant: Hệ thống đã được bật.")
-    assistantSpeak("Hệ thống đã được bật.")
-    query = userCommand().lower()
 
-    while True:
-        print("User: " + query)
-        if (("nhóm 4" in query) or ("nhóm bốn" in query) or ("54" in query)):
-            assistantSpeak("Tôi nghe đây.")
-            query = userCommand().lower()
-            print("User: " + query)
-            if checkEnService(query):
-                answer = "Xin lỗi tôi không hiểu tiếng anh."
-            else:
-                answer = assistantModel(query)
-            assistantSpeak(answer)
-            print("Assistant: " + answer)
 
-            if answer == "Tạm biệt và hẹn gặp lại.":
-                print("Assistant: Hệ thống đã được tắt.")
-                assistantSpeak("Hệ thống đã được tắt.")
-                return
-        else:
-            query = userCommand().lower()
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(webSocketService, "192.168.0.4", 8080)
+)
 
-if __name__ == "__main__":
-    while True:
-        if keyboard.read_key() == "space":
-            assistant()
-        if keyboard.read_key() == "esc":
-            break
+
+asyncio.get_event_loop().run_forever()
